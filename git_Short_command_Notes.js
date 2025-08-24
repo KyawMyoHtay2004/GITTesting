4ab628754a89dd7c1745503d21dfc9c93a663e87
Kyaw Myo Htay <kyawmyohtay2004@gmail.com>
2025-08-24 20:33:30 +0630

    first commit

---
+++ git_Short_command_Notes.js
@@ -0,0 +1,30 @@
+// Github Commands
+ls// List files in the current directory(Linux)
+dir// List files in the current directory(Windows)
+
+cd// Change directory
+cd..// Move up one directory
+
+clear// Clear the terminal screen (Linux)
+cls// Clear the terminal screen (Windows)
+exit// Exit the terminal
+
+mkdir// Create a new directory
+
+code// Open Visual Studio Code
+code .// Open current directory in Visual Studio Code -->
+
+//----------------------------------------------------------------------
+// Modified
+git init// Initialize a new Git repository
+dir -force// List all files including hidden files (Windows)
+dir -a// List all files including hidden files (Linux)
+
+git status// Show the working tree status
+
+// staging
+git add "<file>" .// Stage all changes for the next commit
+git add .// Stage all changes for the next commit
+
+git rm --cached "<file>"// Unstage a file from the staging area
+
